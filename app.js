86c3bda0e2c253b86fde79083c4f77bf21f23621
saman-alayee <saman.alaii10@gmail.com>
2023-08-02 16:40:04 +0330

    router

--- app.js
+++ app.js
@@ -37,11 +37,7 @@ app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
 
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("wrong  route");
-  res.send(course);
-});
+
 app.post("/api/courses", (req, res) => {
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
@@ -61,6 +57,10 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
-
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("wrong  route");
+  res.send(course);
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("port:" + port));
