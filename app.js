2a37462f9a818034ed07e9eadfc89ca6aecb97ed
saman-alayee <saman.alaii10@gmail.com>
2023-08-06 12:06:41 +0330

    set genres

--- app.js
+++ app.js
@@ -1,66 +1,17 @@
 const express = require("express");
 const app = express();
-const Joi = require("joi");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const config = require("config");
-const logger = require('./logger') 
-const startupDebugger = require("debug")("app:startup");
-const dbDebugger = require("debug")("app:db");
+const genres = require('./routes/genres')
+const mongoose = require("mongoose");
 
-app.use(logger)
 app.use(express.json());
-app.use(helmet());
-app.use(morgan("tiny"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use('/api/genres',genres)
 
-// Configuration 
-console.log('app name :'+ config.get('name'));
-console.log('mail server : ' + config.get('name'))
-
-if (app.get("env") === "development") {
-  app.use(morgan("tiny"));
-  console.log(app.get("env"));
-}
-
-const courses = [
-  { id: 1, name: "course1" },
-  { id: 2, name: "course2" },
-  { id: 3, name: "course3" },
-];
-app.get("/", (req, res) => {
-  res.send("<b>home</b>");
-});
-
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
-});
+// connect to database
+mongoose
+  .connect("mongodb://127.0.0.1:27017/genres")
+  .then(() => console.log("connect to mongodb"))
+  .catch((err) => console.log("could not connect to mongodb" + err));
 
 
-app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-  const result = schema.validate({ name: req.body.name });
-  console.log(result);
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  courses.push(course);
-  res.send(course);
-});
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("wrong  route");
-  res.send(course);
-});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("port:" + port));
