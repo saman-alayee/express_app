18a5dddcb7b0be965ed3b158a8d9f10db0f25fe0
saman-alayee <saman.alaii10@gmail.com>
2023-08-07 16:56:42 +0330

    hased password ,repair duplicate key password error

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error);
+
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("user is already register");
 
-  user = new User(_.pick(req.body, ['name','email','password']));
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  // this line hashed password and repair error for duplicate key on password
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
   await user.save();
   res.send(_.pick(user, ["_id", "name", "email"]));
 });
