a60d28ed3991910284fc64b22c324fff5d261a1a
saman-alayee <saman.alaii10@gmail.com>
2023-08-09 17:44:08 +0330

    winston

--- routes/customers.js
+++ routes/customers.js
@@ -1,88 +1,70 @@
+const { Customer, validate } = require("../models/customer");
+const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const Joi = require("joi");
-const auth = require("../middleware/auth")
+const auth = require("../middleware/auth");
 
-const Customer = mongoose.model(
-  "Customer",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-    isGold: {
-      type: Boolean,
-      default: false,
-    },
-    phone: {
-      type: require,
-      type: String,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
-
-router.get("/",auth, async (req, res) => {
+router.get("/", auth, async (req, res) => {
   const customers = await Customer.find().sort("name");
   res.send(customers);
 });
 
-// Post
-router.post("/", async (req, res) => {
-  const { error } = validateCustomer(req.body);
-  if (error) return res.status(400).send(error);
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   let customer = new Customer({
     name: req.body.name,
-    phone: req.body.phone,
     isGold: req.body.isGold,
+    phone: req.body.phone,
   });
   customer = await customer.save();
+
   res.send(customer);
 });
-// Put
-// router.put("/:id", async (req, res) => {
-//   const { error } = validateCustomer(req.body.name);
-//   if (error) return res.status(400).send(error);
-//   const customer = await Customer.findByAndUpdate(
-//     req.params.id,
-//     { name: req.body.name },
-//     { new: true }
-//   );
-//   if (!customer) res.status(404).send("wrong route");
-//   customer.name = req.body.name;
-//   res.send(customer);
-// });
-// // Delete
-// router.delete("/:id", async (req, res) => {
-//   const customer = await Customer.findByAndRemove(
-//     req.params.id,
-//     { name: req.body.name },
-//     { new: true }
-//   );
 
-//   if (!customer) res.status(404).send("wrong route");
-//   res.send(customer);
-// });
-// // Get
-// router.get("/:id", async (req, res) => {
-//   const customer = await Customer.findById(req.params.id);
-//   if (!customer) res.status(404).send("wrong route");
-//   res.send(customer);
-// });
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-function validateCustomer(customer) {
-  const schema = Joi.object({
-    name: Joi.string().min(5).max(50).required(),
-    phone: Joi.string().min(5).max(50).required(),
-    isGold: Joi.boolean(),
-  });
-  const result = schema.validate(customer);
-  return result;
-}
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      isGold: req.body.isGold,
+      phone: req.body.phone,
+    },
+    { new: true }
+  );
+
+  if (!customer)
+    return res
+      .status(404)
+      .send("The customer with the given ID was not found.");
+
+  res.send(customer);
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+
+  if (!customer)
+    return res
+      .status(404)
+      .send("The customer with the given ID was not found.");
+
+  res.send(customer);
+});
+
+router.get("/:id", async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer)
+    return res
+      .status(404)
+      .send("The customer with the given ID was not found.");
+
+  res.send(customer);
+});
 
 module.exports = router;
